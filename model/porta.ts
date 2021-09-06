5c4db13b5e2cf5cf705c0d89886c5faf29b756a3
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-09-06 13:52:29 -0300

    Adicionando Eventos a Porta #02

--- model/porta.ts
+++ model/porta.ts
@@ -9,44 +9,44 @@ export default class PortaModel {
 
     // Método construtor (POO)
     constructor(numero: number, temPresente = false, selecionada = false, aberta = false) {
-        this.#numero = numero
-        this.#temPresente = temPresente
-        this.#selecionada = selecionada
-        this.#aberta = aberta
+        this.#numero = numero;
+        this.#temPresente = temPresente;
+        this.#selecionada = selecionada;
+        this.#aberta = aberta;
     }
 
     // Acessando valores privados
     get numero() {
-        return this.#numero
+        return this.#numero;
     }
 
     get temPresente() {
-        return this.#temPresente
+        return this.#temPresente;
     }
 
     get selecionada() {
-        return this.#selecionada
+        return this.#selecionada;
     }
 
     get aberta() {
-        return this.#aberta
+        return this.#aberta;
     }
 
     // Tornando o modelo da classe Rico => forma funcional
     // Gera uma cópia do objeto, tornando os valores imutáveis
 
     desselecionar() {
-        const selecionada = false
-        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
+        const selecionada = false;
+        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta);
     }
 
     alterarSelecao() {
-        const selecionada = !this.selecionada
-        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
+        const selecionada = !this.selecionada;
+        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta);
     }
 
     abrir() {
-        const aberta = true
-        return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta)
+        const aberta = true;
+        return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta);
     }
 }
