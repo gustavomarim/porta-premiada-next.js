c3be44a3b21b8071ba7bc9f7e4734db9a5c5196b
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-09-06 18:20:01 -0300

    Componente Jogo #01

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,10 @@ import PortaModel from "../model/porta";
 
 export default function Home() {
 
-  const [portas, setPortas] = useState(criarPortas(4, 3));
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta}
-        onChange={novaPorta => {
-          setPortas(atualizarPortas(portas, novaPorta));
-        }} />
-    });
-  };
 
   return (
     <div style={{ display: "flex" }}>
-      {renderizarPortas()}
+      <h1>Formulário de Início </h1>
     </div>
   );
 }
