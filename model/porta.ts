46f02bb1b632bca3c2205f5036e2c6b1d48379d5
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-09-06 12:27:56 -0300

    Integrando TypeScript

--- model/porta.ts
+++ model/porta.ts
@@ -1,14 +1,14 @@
 // Classe que representa dados de uma porta
 
-export default class Porta {
+export default class PortaModel {
     // atributos privados
-    #numero
-    #temPresente
-    #selecionada
-    #aberta
+    #numero: number
+    #temPresente: boolean
+    #selecionada: boolean
+    #aberta: boolean
 
     // Método construtor (POO)
-    constructor(numero, temPresente = false, selecionada = false, aberta = false) {
+    constructor(numero: number, temPresente = false, selecionada = false, aberta = false) {
         this.#numero = numero
         this.#temPresente = temPresente
         this.#selecionada = selecionada
@@ -31,22 +31,22 @@ export default class Porta {
     get aberta() {
         return this.#aberta
     }
-    
+
     // Tornando o modelo da classe Rico => forma funcional
     // Gera uma cópia do objeto, tornando os valores imutáveis
-    
+
     desselecionar() {
         const selecionada = false
-        return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
     alterarSelecao() {
         const selecionada = !this.selecionada
-        return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
     abrir() {
         const aberta = true
-        return new Porta(this.numero, this.temPresente, this.selecionada, aberta)
+        return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta)
     }
 }
